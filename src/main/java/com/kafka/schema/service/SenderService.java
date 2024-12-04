8072b4a74f4ee0602183cfad37c9141a42ad6c55
Vineesh Velayudhan <69500091+Vineesh028@users.noreply.github.com>
2024-12-04 06:40:43 -0800

    Kafka with schema registry

---
+++ src/main/java/com/kafka/schema/service/SenderService.java
@@ -0,0 +1,39 @@
+package com.kafka.schema.service;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+import com.kafka.schema.model.User;
+
+@Service
+public class SenderService {
+
+	@Value("${topic.name}")
+	private String topicName;
+
+	@Autowired
+	private KafkaTemplate<String, User> template;
+
+	public String send(User user) throws InterruptedException, ExecutionException {
+		CompletableFuture<SendResult<String, User>> future = template.send(topicName, UUID.randomUUID().toString(),
+				user);
+
+		return future.handle((result, ex) -> {
+			if (ex == null) {
+
+				return "Sent message=" + user + " to topic= " + result.getRecordMetadata().topic();
+			} else {
+				return "Failed to send message=" + user + " due to : " + ex.getMessage();
+			}
+		}).get();
+
+	}
+
+}
